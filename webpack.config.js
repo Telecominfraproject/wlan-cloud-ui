f2714184b7edfb7951442c564da4f9a9a71a3776
Sean Macfarlane <sean@Seans-MacBook-Pro-2.local>
2020-03-09 16:20:26 -0400

    running

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
-const TerserPlugin = require('terser-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './app/index.js',
   output: {
-    path: `${__dirname  }/dist`,
+    path: path.join(__dirname, 'dist'),
     publicPath: '/',
     filename: 'bundle.js',
   },
   devServer: {
-    contentBase: './dist',
+    port: 3000,
   },
   module: {
     rules: [
@@ -17,14 +19,40 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader', 'eslint-loader'],
       },
+      {
+        test: /\.(css|scss)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              discardDuplicates: true,
+              importLoaders: 1,
+              // This enables local scoped CSS based in CSS Modules spec
+              modules: true,
+              // generates a unique name for each class (e.g. app__app___2x3cr)
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+              sourceMap: process.env.NODE_ENV !== 'production',
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: process.env.NODE_ENV !== 'production',
+            },
+          },
+        ],
+      },
     ],
   },
-  mode: 'production',
   optimization: {
-    minimizer: [
-      new TerserPlugin({
-        /* additional options here */
-      }),
-    ],
+    minimizer: [new UglifyJsPlugin()],
   },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './index.html',
+      filename: './index.html',
+    }),
+  ],
 };
