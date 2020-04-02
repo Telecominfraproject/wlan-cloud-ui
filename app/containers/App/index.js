099babab535f11579e0a063c267f1f09e6856c27
Sean Macfarlane <sean.g.macfarlane@gmail.com>
2020-04-02 17:28:01 -0400

    ThemeProvider set up

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Switch, Route } from 'react-router-dom';
 
-import { Login, Dashboard } from 'cu-ui';
+import { ThemeProvider, Login, Dashboard } from 'cu-ui';
+
+import logo from 'images/logo-light.png';
+import logoMobile from 'images/logoxmobile.jpg';
 
 import ProtectedRouteWithLayout from './components/ProtectedRouteWithLayout';
 
 const App = () => (
-  <>
+  <ThemeProvider company="ConnectUs" logo={logo} logoMobile={logoMobile}>
     <Helmet titleTemplate="%s - ConnectUs" defaultTitle="ConnectUs">
       <meta name="description" content="ConnectUs" />
     </Helmet>
@@ -16,7 +19,7 @@ const App = () => (
       <Route exact path="/login" component={Login} />
       <ProtectedRouteWithLayout exact path="/" component={Dashboard} />
     </Switch>
-  </>
+  </ThemeProvider>
 );
 
 export default App;
--- app/containers/MasterLayout/index.js
+++ app/containers/MasterLayout/index.js
@@ -4,13 +4,10 @@ import { useLocation } from 'react-router-dom';
 
 import { AppLayout as Layout } from 'cu-ui';
 
-import logo from 'images/logo-light.png';
-import logoMobile from 'images/logoxmobile.jpg';
-
 const MasterLayout = ({ children }) => {
   const location = useLocation();
   return (
-    <Layout company="ConnectUs" logo={logo} logoMobile={logoMobile} onLogout={() => {}} locationState={location}>
+    <Layout onLogout={() => {}} locationState={location}>
       {children}
     </Layout>
   );
