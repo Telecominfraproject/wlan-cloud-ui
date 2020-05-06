bf72b9d2c8c2649065e491d1bd82806930f9e5eb
Irtiza-h30 <ihasa074@uottawa.ca>
2020-05-06 16:40:56 -0400

    Created Edit Account

---
+++ app/containers/EditAccount/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { EditAccount as EditAccountPage } from '@tip-wlan/wlan-cloud-ui-library';
+
+const EditAccount = () => {
+  return <EditAccountPage />;
+};
+
+export default EditAccount;
