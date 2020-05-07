d7d2837b81ccc134768e72704b2d8b1eb1f6b130
Irtiza-h30 <ihasa074@uottawa.ca>
2020-05-07 15:42:36 -0400

    Passed handleSubmit and Email props

--- app/containers/EditAccount/index.js
+++ app/containers/EditAccount/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { EditAccount as EditAccountPage } from '@tip-wlan/wlan-cloud-ui-library';
 
 const EditAccount = () => {
-  return <EditAccountPage />;
+  const handleSubmit = () => {};
+
+  return <EditAccountPage onSubmit={handleSubmit} email="" />;
 };
 
 export default EditAccount;
