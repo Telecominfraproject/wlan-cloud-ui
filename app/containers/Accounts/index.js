c79ae344cb628931cbb0e093e51c976dd7331f42
Irtiza-h30 <ihasa074@uottawa.ca>
2020-05-12 17:00:54 -0400

    Added query to attain users

--- app/containers/Accounts/index.js
+++ app/containers/Accounts/index.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+import UserContext from 'contexts/UserContext';
+import { notification, Spin } from 'antd';
+
 import { Accounts as AccountsPage } from '@tip-wlan/wlan-cloud-ui-library';
 
+const GET_ALL_USERS = gql`
+  query GetAllUsers($customerId: Int!) {
+    getAllUsers(customerId: $customerId) {
+      items {
+        id
+        username
+        role
+      }
+    }
+  }
+`;
 export default function Accounts() {
+  const { customerId } = useContext(UserContext);
+  const { loading, error, data } = useQuery(GET_ALL_USERS, { variables: { customerId } });
+
+  if (loading) {
+    return <Spin size="large" />;
+  }
+
+  if (error) {
+    notification.error({
+      message: 'Error',
+      description: 'Failed to load users',
+    });
+  }
+
   return (
     <div>
-      <AccountsPage />
+      <AccountsPage data={data} />
     </div>
   );
 }
