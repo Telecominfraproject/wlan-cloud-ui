5e8db4f7056686ee44576ff95c321bcff16c6315
Irtiza-h30 <ihasa074@uottawa.ca>
2020-05-28 17:58:31 -0400

    Initial commit

--- app/containers/Alarms/index.js
+++ app/containers/Alarms/index.js
@@ -1,5 +1,84 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+import { Alert, Spin, notification } from 'antd';
+import { Alarms as AlarmsPage } from '@tip-wlan/wlan-cloud-ui-library';
+import UserContext from 'contexts/UserContext';
 
-const Alarms = () => <h1>Alarms</h1>;
+const GET_ALL_ALARMS = gql`
+  query GetAllAlarms($customerId: Int!, $cursor: String) {
+    getAllAlarms(customerId: $customerId, cursor: $cursor) {
+      items {
+        severity
+        alarmCode
+        details
+        createdTimestamp
+      }
+      context {
+        cursor
+        lastPage
+      }
+    }
+  }
+`;
+
+const Alarms = () => {
+  const { customerId } = useContext(UserContext);
+  const { loading, error, data, refetch, fetchMore } = useQuery(GET_ALL_ALARMS, {
+    variables: { customerId },
+  });
+
+  const reloadTable = () => {
+    refetch()
+      .then(() => {
+        notification.success({
+          message: 'Success',
+          description: 'Alarms reloaded.',
+        });
+      })
+      .catch(() =>
+        notification.error({
+          message: 'Error',
+          description: 'Alarms could not be reloaded.',
+        })
+      );
+  };
+
+  const handleLoadMore = () => {
+    if (!data.getAllAlarms.context.lastPage) {
+      fetchMore({
+        variables: { cursor: data.getAllAlarms.context.cursor },
+        updateQuery: (previousResult, { fetchMoreResult }) => {
+          const previousEntry = previousResult.getAllAlarms;
+          const newItems = fetchMoreResult.getAllAlarms.items;
+
+          return {
+            getAllAlarms: {
+              context: fetchMoreResult.getAllAlarms.context,
+              items: [...previousEntry.items, ...newItems],
+              __typename: previousEntry.__typename,
+            },
+          };
+        },
+      });
+    }
+  };
+
+  if (loading) {
+    return <Spin size="large" />;
+  }
+
+  if (error) {
+    return <Alert message="Error" description="Failed to load alarms." type="error" showIcon />;
+  }
+  return (
+    <AlarmsPage
+      data={data.getAllAlarms.items}
+      onReload={reloadTable}
+      onLoadMore={handleLoadMore}
+      isLastPage={data.getAllAlarms.context.lastPage}
+    />
+  );
+};
 
 export default Alarms;
