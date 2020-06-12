ce7c501f52e2fbb37063a32bfe961ede1be821b8
Ali Muhammad <ali.muhammad@connectus.ai>
2020-06-12 18:00:06 -0400

    add one component for all device details and fixed css

--- app/containers/Network/containers/ClientDeviceDetails/index.js
+++ app/containers/Network/containers/ClientDeviceDetails/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 import { Alert } from 'antd';
@@ -10,10 +10,123 @@ import UserContext from 'contexts/UserContext';
 import { GET_CLIENT_SESSION } from 'graphql/queries';
 
 const ClientDeviceDetails = () => {
+  const [generalCardState, setGeneralcardState] = useState([
+    {
+      Status: '',
+      'Associated On': '',
+      'Access Point': '',
+      SSID: '',
+      'Radio Band': '',
+      'Signal Strength': '',
+      'Tx Rate': '',
+      'Rx Rate': '',
+    },
+  ]);
+  const [trafficCardState, setTrafficcardState] = useState([
+    {
+      'Data Transferred': '',
+      'Tx Throughput': '',
+      'Rx Throughput': '',
+      'Total Tx Packets': '',
+      'Total Rx Packets': '',
+    },
+  ]);
+  const [ipLanCardState, setIpLancardState] = useState([
+    {
+      'IPv4 Address': '',
+      'DHCP Server': '',
+      'Primary DNS': '',
+      'Secondary DNS': '',
+      'Gateway ': '',
+      'Subnet Mask': '',
+      'IP Lease Time': '',
+      'IP Lease Start': '',
+    },
+  ]);
+
+  const [detailsObject, setDetailsObject] = useState({
+    macAddress: '',
+    ipAddress: '',
+    hostName: '',
+    ssid: '',
+    radioType: '',
+    signal: '',
+    equipmentName: '',
+  });
+
   const { id } = useParams();
   const { customerId } = useContext(UserContext);
   const { loading, error, data } = useQuery(GET_CLIENT_SESSION, {
     variables: { customerId, macAddress: id },
+    onCompleted: () => {
+      const {
+        macAddress,
+        ipAddress,
+        hostName,
+        ssid,
+        radioType,
+        signal,
+        equipment: { name },
+        details: {
+          assocTimestamp,
+          dhcpDetails,
+          metricDetails: { rxBytes, txBytes, rxMbps, txMbps, totalRxPackets, totalTxPackets },
+        },
+      } = data.getClientSession[0];
+
+      const {
+        dhcpServerIp,
+        primaryDns,
+        secondaryDns,
+        gatewayIp,
+        subnetMask,
+        leaseTimeInSeconds,
+        leaseStartTimestamp,
+      } = dhcpDetails || {};
+
+      setDetailsObject({
+        macAddress,
+        ipAddress,
+        hostName,
+        radioType,
+        signal,
+        equipmentName: name,
+      });
+
+      setGeneralcardState([
+        {
+          Status: '',
+          'Associated On': assocTimestamp,
+          'Access Point': name,
+          SSID: ssid,
+          'Radio Band': radioType,
+          'Signal Strength': signal,
+          'Tx Rate': rxMbps,
+          'Rx Rate': txMbps,
+        },
+      ]);
+      setTrafficcardState([
+        {
+          'Data Transferred': '',
+          'Tx Throughput': txBytes,
+          'Rx Throughput': rxBytes,
+          'Total Tx Packets': totalTxPackets,
+          'Total Rx Packets': totalRxPackets,
+        },
+      ]);
+      setIpLancardState([
+        {
+          'IPv4 Address': '',
+          'DHCP Server': dhcpServerIp,
+          'Primary DNS': primaryDns,
+          'Secondary DNS': secondaryDns,
+          'Gateway ': gatewayIp,
+          'Subnet Mask': subnetMask,
+          'IP Lease Time': leaseTimeInSeconds,
+          'IP Lease Start': leaseStartTimestamp,
+        },
+      ]);
+    },
   });
 
   if (loading) {
@@ -25,67 +138,23 @@ const ClientDeviceDetails = () => {
       <Alert message="Error" description="Failed to load Client Device." type="error" showIcon />
     );
   }
-
-  const {
-    macAddress,
-    ipAddress,
-    hostname,
-    ssid,
-    radioType,
-    signal,
-    equipment: { name },
-    details: {
-      assocTimestamp,
-      dhcpDetails,
-      metricDetails: { rxBytes, txBytes, rxMbps, txMbps, totalRxPackets, totalTxPackets },
-    },
-  } = data.getClientSession[0];
-
-  /*
- assocTimestamp = Associated On
- equipment.name = Access Point
- radioType = Radio Band
- signal = Signal Strength
- rxMbps = Rx Rate
- txMbps = Tx Rate
-
- rxBytes = Rx Throughput
- txBytes = Tx Throughput
- totalRxPackets = Total Rx Packets
- totalTxPackets = Total Tx Packets
- 
- dhcpServerIp = DHCP Server
- primaryDns = Primary DNS
- secondaryDns = Secondary DNS
- gatewayIp = Gateway
- subnetMask = Subnet Mask
- leaseTimeInSeconds = IP Lease Time
- leaseStartTimestamp = IP Lease Start
- */
+  const { macAddress, ipAddress, hostName, radioType, signal, equipmentName } = detailsObject;
 
   return (
     <>
       <ClientDevicesDetails
         macAddress={macAddress}
         ipAddress={ipAddress}
-        hostname={hostname}
-        ssid={ssid}
+        hostname={hostName}
         radioType={radioType}
         signal={signal}
-        name={name}
-        assocTimestamp={assocTimestamp}
-        rxMbps={rxMbps}
-        txMbps={txMbps}
-        totalRxPackets={totalRxPackets}
-        totalTxPackets={totalTxPackets}
-        dhcpDetails={dhcpDetails}
-        rxBytes={rxBytes}
-        txBytes={txBytes}
+        name={equipmentName}
+        generalCardState={generalCardState}
+        trafficCardState={trafficCardState}
+        ipLanCardState={ipLanCardState}
       />
     </>
   );
 };
-
-ClientDeviceDetails.propTypes = {};
 
 export default ClientDeviceDetails;
