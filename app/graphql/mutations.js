b5c7ad0a7bb849d4bd65254eccbe4daf2f0742ab
Sean Macfarlane <sean.g.macfarlane@gmail.com>
2020-07-09 13:06:53 -0400

    switched apollo boost to apollo client

--- app/graphql/mutations.js
+++ app/graphql/mutations.js
@@ -1,5 +1,15 @@
 import gql from 'graphql-tag';
 
+export const REFRESH_TOKEN = gql`
+  mutation UpdateToken($refreshToken: String!) {
+    updateToken(refreshToken: $refreshToken) {
+      access_token
+      refresh_token
+      expires_in
+    }
+  }
+`;
+
 export const CREATE_LOCATION = gql`
   mutation CreateLocation(
     $locationType: String!
@@ -67,7 +77,8 @@ export const UPDATE_EQUIPMENT_BULK = gql`
 export const FILE_UPLOAD = gql`
   mutation FileUpload($fileName: String, $file: Upload) {
     fileUpload(fileName: $fileName, file: $file) {
-      success
+      fileName
+      baseUrl
     }
   }
 `;
