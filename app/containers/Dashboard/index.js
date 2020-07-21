df47bd0b7ea86c1d3af2a9b2fb4772b359bc6ba0
Alidev123 <ali.muhammad@connectus.ai>
2020-07-21 11:17:29 -0400

    added linechart component to dashboard

--- app/containers/Dashboard/index.js
+++ app/containers/Dashboard/index.js
@@ -1,10 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { Alert } from 'antd';
 import { Dashboard as DashboardPage, Loading } from '@tip-wlan/wlan-cloud-ui-library';
-
+import moment from 'moment';
 import UserContext from 'contexts/UserContext';
-import { GET_ALL_STATUS } from 'graphql/queries';
+import { GET_ALL_STATUS, FILTER_SYSTEM_EVENTS } from 'graphql/queries';
 
 function formatBytes(bytes, decimals = 2) {
   if (bytes === 0) return '0 Bytes';
@@ -24,14 +24,86 @@ const Dashboard = () => {
   const { loading, error, data } = useQuery(GET_ALL_STATUS, {
     variables: { customerId, statusDataTypes: ['CUSTOMER_DASHBOARD'] },
   });
+  const [toTime] = useState(
+    moment()
+      .valueOf()
+      .toString()
+  );
+  const [fromTime] = useState(
+    moment()
+      .subtract(24, 'days')
+      .valueOf()
+      .toString()
+  );
 
-  const titleList = ['Access Points', 'Client Devices', 'Usage Information'];
+  const {
+    loading: metricsLoading,
+    error: metricsError,
+    data: metricsData,
+    // refetch: metricsRefetch,
+  } = useQuery(FILTER_SYSTEM_EVENTS, {
+    variables: {
+      customerId,
+      fromTime,
+      toTime,
+      equipmentIds: [0],
+      dataTypes: ['StatusChangedEvent'],
+      limit: 100,
+    },
+  });
 
-  if (loading) {
+  const pieChartTitle = ['Access Points', 'Client Devices', 'Usage Information'];
+  const lineChartTitle = [
+    'Inservice APs (24hours)',
+    'Client Devices (24hours)',
+    'Traffic (24hours)',
+  ];
+
+  const [lineChartXAxis] = useState([]);
+  const [lineChartYAxis] = useState({
+    inservicesAPs: { name: 'Inservice APs', value: [] },
+    clientDevices: {
+      is2dot4GHz: { name: '2.4GHz', value: [] },
+      is5GHzL: { name: '5GHz (L)', value: [] },
+      is5GHzU: { name: '5GHz (U)', value: [] },
+    },
+    traffic: {
+      trafficBytesDownstream: { name: 'Down Stream', value: [] },
+      trafficBytesUpstream: { name: 'Up Stream', value: [] },
+    },
+  });
+
+  useEffect(() => {
+    return metricsData?.filterSystemEvents?.items?.forEach(
+      ({
+        eventTimestamp,
+        details: {
+          payload: {
+            details: {
+              equipmentInServiceCount,
+              associatedClientsCountPerRadio: { is2dot4GHz, is5GHzL, is5GHzU },
+              trafficBytesDownstream,
+              trafficBytesUpstream,
+            },
+          },
+        },
+      }) => {
+        lineChartXAxis.push(eventTimestamp);
+        lineChartYAxis.inservicesAPs.value.push(equipmentInServiceCount);
+        lineChartYAxis.clientDevices.is2dot4GHz.value.push(is2dot4GHz);
+        lineChartYAxis.clientDevices.is5GHzL.value.push(is5GHzL);
+        lineChartYAxis.clientDevices.is5GHzU.value.push(is5GHzU);
+        lineChartYAxis.traffic.trafficBytesDownstream.value.push(trafficBytesDownstream);
+        lineChartYAxis.traffic.trafficBytesUpstream.value.push(trafficBytesUpstream);
+      }
+    );
+  }, [metricsLoading]);
+
+  if (loading || metricsLoading) {
     return <Loading />;
   }
 
-  if (error) {
+  if (error || metricsError) {
     return <Alert message="Error" description="Failed to load Dashboard" type="error" showIcon />;
   }
 
@@ -80,8 +152,16 @@ const Dashboard = () => {
     },
   ];
   const pieChartData = [equipmentCountPerOui, clientCountPerOui];
-
-  return <DashboardPage titleList={titleList} statsArr={statsArr} pieChartData={pieChartData} />;
+  return (
+    <DashboardPage
+      pieChartTitle={pieChartTitle}
+      statsArr={statsArr}
+      pieChartData={pieChartData}
+      lineChartYAxis={lineChartYAxis}
+      lineChartXAxis={lineChartXAxis}
+      lineChartTitle={lineChartTitle}
+    />
+  );
 };
 
 export default Dashboard;
--- app/graphql/queries.js
+++ app/graphql/queries.js
@@ -229,3 +229,31 @@ export const GET_ALARM_COUNT = gql`
     getAlarmCount(customerId: $customerId)
   }
 `;
+
+export const FILTER_SYSTEM_EVENTS = gql`
+  query FilterSystemEvents(
+    $customerId: ID!
+    $fromTime: String!
+    $toTime: String!
+    $equipmentIds: [ID]
+    $dataTypes: [String]
+    $cursor: String
+    $limit: Int
+  ) {
+    filterSystemEvents(
+      customerId: $customerId
+      fromTime: $fromTime
+      toTime: $toTime
+      dataTypes: $dataTypes
+      equipmentIds: $equipmentIds
+      cursor: $cursor
+      limit: $limit
+    ) {
+      items
+      context {
+        lastPage
+        cursor
+      }
+    }
+  }
+`;
