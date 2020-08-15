5854c1a0a040ec3b2fa3cd6fb1cde8cf3c8810d5
Irtiza-h30 <ihasa074@uottawa.ca>
2020-08-14 23:43:50 -0400

    updated query

--- app/containers/Network/containers/AccessPointDetails/index.js
+++ app/containers/Network/containers/AccessPointDetails/index.js
@@ -1,8 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
 import gql from 'graphql-tag';
-import { useQuery, useMutation } from '@apollo/react-hooks';
+import { useQuery, useMutation, useLazyQuery } from '@apollo/react-hooks';
 import { Alert, notification } from 'antd';
 import moment from 'moment';
 import {
@@ -149,14 +149,27 @@ const AccessPointDetails = ({ locations }) => {
   const { customerId } = useContext(UserContext);
 
   const { loading, error, data, refetch } = useQuery(GET_EQUIPMENT, {
-    variables: { id },
+    variables: {
+      id,
+    },
   });
-  const { data: dataProfiles, error: errorProfiles, loading: landingProfiles } = useQuery(
+
+  const [
+    getAllFirmware,
+    { data: dataFirmware, error: errorFirmware, loading: loadingFirmware },
+  ] = useLazyQuery(GET_ALL_FIRMWARE);
+
+  useEffect(() => {
+    getAllFirmware({ variables: { modelId: data?.getEquipment?.model?.toLowerCase() } });
+  }, [data]);
+
+  const { data: dataProfiles, error: errorProfiles, loading: loadingProfiles } = useQuery(
     GET_ALL_PROFILES,
     {
       variables: { customerId, type: 'equipment_ap', limit: 100 },
     }
   );
+
   const {
     loading: metricsLoading,
     error: metricsError,
@@ -175,13 +188,6 @@ const AccessPointDetails = ({ locations }) => {
 
   const [updateEquipment] = useMutation(UPDATE_EQUIPMENT);
   const [updateEquipmentFirmware] = useMutation(UPDATE_EQUIPMENT_FIRMWARE);
-
-  const { data: dataFirmware, error: errorFirmware, loading: landingFirmware } = useQuery(
-    GET_ALL_FIRMWARE,
-    {
-      variables: { modelId: data?.getEquipment?.model },
-    }
-  );
 
   const refetchData = () => {
     refetch();
@@ -255,7 +261,7 @@ const AccessPointDetails = ({ locations }) => {
         })
       );
 
-  if (loading || landingProfiles || landingFirmware) {
+  if (loading || loadingProfiles || loadingFirmware) {
     return <Loading />;
   }
 
@@ -296,14 +302,14 @@ const AccessPointDetails = ({ locations }) => {
     <AccessPointDetailsPage
       handleRefresh={refetchData}
       onUpdateEquipment={handleUpdateEquipment}
-      data={data.getEquipment}
-      profiles={dataProfiles.getAllProfiles.items}
+      data={data?.getEquipment}
+      profiles={dataProfiles?.getAllProfiles?.items}
       osData={{
         loading: metricsLoading,
         error: metricsError,
         data: metricsData && metricsData.filterServiceMetrics.items,
       }}
-      firmware={dataFirmware.getAllFirmware}
+      firmware={dataFirmware?.getAllFirmware}
       locations={locations}
       onUpdateEquipmentFirmware={handleUpdateEquipmentFirmware}
     />
